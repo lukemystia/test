d624182a77ec4fb08d235122a0646da8e11df70f
lukemystia <komeizi.satori@gmail.com>
2016-07-02 09:20:21 +0900

    Add files via upload

---
+++ DigitalClock2/DigitalClock2/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace DigitalClock2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.WindowText;
+			this.ClientSize = new System.Drawing.Size(226, 70);
+			this.ControlBox = false;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DigitalClock2/DigitalClock2/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace DigitalClock2
+{
+	public partial class Form1 : Form
+	{
+		//マウスクリック位置記憶
+		private Point mousePoint;
+		//日本表記
+		private CultureInfo jpCluture = new CultureInfo("ja-JP");
+		//表示フォント
+		Font f = new Font("MS ゴシック", 16);
+		//曜日表示位置
+		Rectangle weekDayPosition = new Rectangle(140, 37, 300, 35);
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			Screen[] allScreens = Screen.AllScreens;
+
+			int screenWidthAll = 0;
+			foreach (var items in allScreens)
+			{
+				screenWidthAll += items.Bounds.Width;
+			}
+
+			//魏同時の位置設定
+			//起動時前回の位置に表示 ただし画面外の時は修正
+			if (Properties.Settings.Default.MainForm_Left < 0 ||
+				Properties.Settings.Default.MainForm_Left > screenWidthAll)
+			{
+				this.Left = 500;
+			}
+			else
+			{
+				this.Left = Properties.Settings.Default.MainForm_Left;
+			}
+
+			if (Properties.Settings.Default.MainForm_Top < 0 ||
+				Properties.Settings.Default.MainForm_Top > System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height)
+			{
+				this.Top = 500;
+			}
+			else
+			{
+				this.Top = Properties.Settings.Default.MainForm_Top;
+			}
+
+			//日本表記用
+			jpCluture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+			this.Refresh();
+		}
+
+		//フォーム上でマウスクリックされた時の処理
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+			{
+				//位置記憶
+				mousePoint = new Point(e.X, e.Y);
+			}
+		}
+
+		//マウスが動いた時の処理
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+			{
+				//位置移動
+				this.Left += e.X - mousePoint.X;
+				this.Top += e.Y - mousePoint.Y;
+			}
+		}
+
+		//ダブルクリックで終了
+		private void Form1_DoubleClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		//timer_Tickイベント(1000ミリ秒ごとに発生)
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			timer1.Stop();
+			this.Refresh();	//再描写
+			timer1.Start();
+		}
+
+		//終了時位置記憶
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			//ウィンドウの値を Settings に格納
+			Properties.Settings.Default.MainForm_Left = this.Left;
+			Properties.Settings.Default.MainForm_Top = this.Top;
+			//ファイルに保存
+			Properties.Settings.Default.Save();
+
+			Application.Exit();
+		}
+
+		//時刻等描写
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			DateTime dt = DateTime.Now;
+
+			e.Graphics.DrawString(dt.ToString("yyyy/MM dd HH:mm:ss"),
+								  f,
+								  Brushes.White,
+								  new Rectangle(12, 9, 300, 35));
+
+			e.Graphics.DrawString(dt.ToString("ggyy年", jpCluture),
+								  f,
+								  Brushes.White,
+								  new Rectangle(12, 37, 110, 35));
+
+			switch ((int)dt.DayOfWeek)
+			{
+				case 0:
+					e.Graphics.DrawString(dt.ToString("dddd", jpCluture),
+								  f,
+								  Brushes.IndianRed,
+								  weekDayPosition);
+					break;
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+					e.Graphics.DrawString(dt.ToString("dddd", jpCluture),
+								  f,
+								  Brushes.White,
+								  weekDayPosition);
+					break;
+				case 6:
+					e.Graphics.DrawString(dt.ToString("dddd", jpCluture),
+								  f,
+								  Brushes.LightBlue,
+								  weekDayPosition);
+					break;
+				default:
+					e.Graphics.DrawString("エラー",
+								  f,
+								  Brushes.Red,
+								  weekDayPosition);
+					break;
+			}
+
+		}
+	}
+}
